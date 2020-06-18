76b6be77d47c0deaab3737e2bfca968ccec998f2
SAHIL SHARMA <shubhamsharma4965@gmail.com>
2020-06-19 00:44:35 +0530

    Updates

---
+++ precache-manifest.295651a279a411564646ec2c2ee3e983.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083cd6bb70d9fa7ff8ca484ca62d2478",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "15dbf2e9ef2090201ef0",
+    "url": "/weather/static/css/main.5fe03ead.chunk.css"
+  },
+  {
+    "revision": "cd047976454f111e2d0c",
+    "url": "/weather/static/js/2.b71ae9db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.b71ae9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dbf2e9ef2090201ef0",
+    "url": "/weather/static/js/main.37d5d7f3.chunk.js"
+  },
+  {
+    "revision": "c3e4f75e9c2ba3be5f23",
+    "url": "/weather/static/js/runtime-main.d26e3fa4.js"
+  },
+  {
+    "revision": "f604b11be294bcca8e251563a766567c",
+    "url": "/weather/static/media/Drizzle.f604b11b.jpg"
+  },
+  {
+    "revision": "ddb8cde43245c536ace8a8d20cbee869",
+    "url": "/weather/static/media/Fog.ddb8cde4.jpg"
+  },
+  {
+    "revision": "e1a769fb34439ae5d12771d8dcb340c0",
+    "url": "/weather/static/media/Snow1.e1a769fb.jpg"
+  },
+  {
+    "revision": "babc4dadea80e7707790465c29a6eeac",
+    "url": "/weather/static/media/Thunderstorm.babc4dad.jpg"
+  },
+  {
+    "revision": "fa22b498cd59127d90d381c0b5912421",
+    "url": "/weather/static/media/clear1.fa22b498.jpg"
+  },
+  {
+    "revision": "c0210a64ba129514e0cc558b2bcca42b",
+    "url": "/weather/static/media/clear2.c0210a64.jpg"
+  },
+  {
+    "revision": "2c8c85da237ea90f8919def96acaf951",
+    "url": "/weather/static/media/clear3.2c8c85da.jpg"
+  },
+  {
+    "revision": "7b73a7f4e87a77b6f928ca39904bb018",
+    "url": "/weather/static/media/clouds1.7b73a7f4.jpg"
+  },
+  {
+    "revision": "e690c64b1be47a544de52179060f4954",
+    "url": "/weather/static/media/clouds2.e690c64b.jpg"
+  },
+  {
+    "revision": "097729083c1544a337cbaf5d6c3a48b6",
+    "url": "/weather/static/media/haze1.09772908.jpg"
+  },
+  {
+    "revision": "1d08e227b55341d0bf962a28131c522f",
+    "url": "/weather/static/media/haze2.1d08e227.jpg"
+  },
+  {
+    "revision": "09448fed3aa5e7a0ee70b2fd020fc9d5",
+    "url": "/weather/static/media/haze3.09448fed.jpg"
+  },
+  {
+    "revision": "3c22cb44aae3f09ac29eab97e2c6368c",
+    "url": "/weather/static/media/rain.3c22cb44.jpg"
+  },
+  {
+    "revision": "62149c89a0fc0f0bc90f55e1dee6fbeb",
+    "url": "/weather/static/media/snow2.62149c89.jpg"
+  },
+  {
+    "revision": "7b7a3b2d25a47101da1fb5405f5d0b06",
+    "url": "/weather/static/media/snow3.7b7a3b2d.jpg"
+  },
+  {
+    "revision": "51cc21eb49cbad9c2142b0d23dd7551e",
+    "url": "/weather/static/media/tornado.51cc21eb.jpg"
+  }
+]);
